1c2ad216ff1b5bd622f9be0d7166facdf2fdf639
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-04 00:04:55 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mysql = require('mysql2');
 
+if (!process.env.MYSQL_URL) {
+  console.error("MYSQL_URL tidak ada!");
+  process.exit(1);
+}
+
 const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
