a6d9b0d272509cabb933d5bea8da3d9ea2c93bdf
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-03 23:15:22 +0700

    Backend Setting For Railway

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
+
+const db = process.env.DATABASE_URL;
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: "https://lauren-note-taking-app.netlify.app"
+}));
+
 app.use(express.json());
 
 const notesRoutes = require('./routes/notes');
 app.use('/notes', notesRoutes);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+// app.listen(5000, () => {
+//   console.log('Server running on port 5000');
+// });
+
+//Untuk Git Railway 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server jalan");
 });
