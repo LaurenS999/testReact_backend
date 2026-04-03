dffeb85d3f4a42aa5bf20531e8a05fa8825f6d8f
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-03 23:37:16 +0700

    Setting Db.js connect ke railway

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'note_taking'
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 
 module.exports = db;
