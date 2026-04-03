fc262f9fe6fde99d6efb84e6259c415b3ad588da
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-04 00:40:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const db = process.env.DATABASE_URL;
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const cors = require('cors');
 
-app.use(cors({
-  origin: "https://lauren-note-taking-app.netlify.app"
-}));
-
+app.use(cors());
 app.use(express.json());
 
-const notesRoutes = require('./routes/notes');
-app.use('/notes', notesRoutes);
+// route test
+app.get("/", (req, res) => {
+  res.send("API jalan");
+});
 
-// app.listen(5000, () => {
-//   console.log('Server running on port 5000');
-// });
-
-//Untuk Git Railway 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server jalan");
+  console.log(`Server jalan di port ${PORT}`);
 });
