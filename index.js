a2e31a887d916e62d466cbe30d9e7e9d6db6c3f5
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-04 01:27:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
   res.send("API jalan 🚀");
 });
 
-// 🔥 FIX: pakai port manual
+// contoh API
+app.get("/api/notes", (req, res) => {
+  res.json({
+    message: "API Notes berhasil",
+  });
+});
+
+// 🔥 PENTING: pakai port 5000 (karena Railway mapping ke 5000)
 const PORT = 5000;
 
-app.listen(5000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server jalan di port ${PORT}`);
 });
