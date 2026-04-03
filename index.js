5fa3f05e153b09d248bc21fe8aedfb5dceb4e2d8
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-04 00:50:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API jalan");
 });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server jalan di port ${PORT}`);
